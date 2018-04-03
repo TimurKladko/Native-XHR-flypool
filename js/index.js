a5b33e62fb70a2db9a4c6e437ec53cc287b1bcc7
TimurKladko <timurkladko@gmail.com>
2018-04-03 22:36:50 +0300

    cut useless code js

--- js/index.js
+++ js/index.js
@@ -1,34 +1,12 @@
 var requests = 0;
 
-var statistic = document
-  .getElementById("button")
-  .addEventListener("click", function() {
+document.getElementById("button").addEventListener("click", function() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
         console.log(JSON.parse(xhttp.responseText).data.currentStatistics);
         requests++;
-        //console.log(requests);
-        // document
-        //   .getElementById("results")
-        //   .innerHTML = (
-        //     "<tr>" +
-        //       "<td>" +
-        //         (JSON.parse(xhttp.responseText).data.currentStatistics.unpaid)
-        //       + "</td>" +
-        //       "<td>" +
-        //         (JSON.parse(xhttp.responseText).data.currentStatistics
-        //           .unconfirmed)
-        //       + "</td>" +
-        //       "<td>" +
-        //         (JSON.parse(xhttp.responseText).data.currentStatistics
-        //           .currentHashrate)
-        //       + "</td>" +
-        //       "<td>" +
-        //         (JSON.parse(xhttp.responseText).data.currentStatistics.time) +
-        //       "</td>" +
-        //       "</tr>"
-        //   );
+
 
         var statistics = JSON.parse(xhttp.responseText).data.currentStatistics;
 
@@ -42,7 +20,6 @@ var statistic = document
         th.innerHTML = requests;
         row.appendChild(th);
 
-        //var cellRequest = row.insertCell();
         var cell1 = row.insertCell(1);
         var cell2 = row.insertCell(2);
         var cell3 = row.insertCell(3);
@@ -50,7 +27,6 @@ var statistic = document
         var cell5 = row.insertCell(5);
         var cell6 = row.insertCell(6);
 
-        //cellRequest.innerHTML = requests;
         cell1.innerHTML = statistics.activeWorkers;
         cell2.innerHTML = (statistics.unpaid * 1e-9).toFixed(5);
         cell3.innerHTML = (statistics.unconfirmed * 1e-9).toFixed(5);
